25037d93c2706d578dc515b33ea1fb626ac7214f
wenkai.ye@tenclass.com <lxyzh181>
2022-06-06 00:51:27 +0800

    添加登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home'
+import Login from '../components/Login'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
             path: '/home',
             name: "Home",
             component: Home,
+        },
+        {
+            path: '/user/login',
+            name: "Login",
+            component: Login,
         }
     ]
 })
